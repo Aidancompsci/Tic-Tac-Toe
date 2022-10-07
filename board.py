5adb51a044ce277c6544f9753e47433b8725a214
Aidancompsci <111991363+Aidancompsci@users.noreply.github.com>
2022-10-07 10:01:18 -0500

    Board change
    Increased the board size by a factor of 2. Started the beginning of creating the square handling.

--- board.py
+++ board.py
@@ -8,20 +8,28 @@ def goto_without_pen(x, y):
     board_turtle.goto(x, y)
     board_turtle.pendown()
 
-#Each square is 50x50
+#Each square is 100100
 def create_board():
     # left line
-    goto_without_pen(-25, 75)
-    board_turtle.goto(-25, -75)
+    goto_without_pen(-50, 150)
+    board_turtle.goto(-50, -150)
     #right line
-    goto_without_pen(25, 75)
-    board_turtle.goto(25, -75)
+    goto_without_pen(50, 150)
+    board_turtle.goto(50, -150)
     #top line
-    goto_without_pen(-75, 25)
-    board_turtle.goto(75, 25)
+    goto_without_pen(-150, 50)
+    board_turtle.goto(150, 50)
     #bottom line
-    goto_without_pen(-75, -25)
-    board_turtle.goto(75, -25)
+    goto_without_pen(-150, -50)
+    board_turtle.goto(150, -50)
 
+
+    squares_list = []
+
+    for i in range(3):
+        for i in range(3):
+            new_turtle = turtle.Turtle()
+            new_turtle.shape("square")
+            new_turtle.color("white")
 
     pass
