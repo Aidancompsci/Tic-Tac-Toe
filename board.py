a3cb42c79dc062eb9f4058fa8c165b2d87d2aa5c
Aidancompsci <111991363+Aidancompsci@users.noreply.github.com>
2022-10-07 11:37:38 -0500

    Lots of stuff
    Added the drawing of x's and o's on clicking.

--- board.py
+++ board.py
@@ -2,34 +2,60 @@ import turtle
 
 board_turtle = turtle.Turtle()
 board_turtle.hideturtle()
+board_turtle.speed(0)
 
-def goto_without_pen(x, y):
-    board_turtle.penup()
-    board_turtle.goto(x, y)
-    board_turtle.pendown()
+def goto_without_pen(turtle, x, y):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+
+def on_click(x,y):
+    print("clicked")
 
 #Each square is 100100
 def create_board():
     # left line
-    goto_without_pen(-50, 150)
+    goto_without_pen(board_turtle, -50, 150)
     board_turtle.goto(-50, -150)
     #right line
-    goto_without_pen(50, 150)
+    goto_without_pen(board_turtle, 50, 150)
     board_turtle.goto(50, -150)
     #top line
-    goto_without_pen(-150, 50)
+    goto_without_pen(board_turtle, -150, 50)
     board_turtle.goto(150, 50)
     #bottom line
-    goto_without_pen(-150, -50)
+    goto_without_pen(board_turtle, -150, -50)
     board_turtle.goto(150, -50)
-
 
     squares_list = []
 
-    for i in range(3):
-        for i in range(3):
+    for j in range(3):
+        for k in range(3):
             new_turtle = turtle.Turtle()
             new_turtle.shape("square")
             new_turtle.color("white")
+            goto_without_pen(new_turtle, (-100 + (100 * j)), (100 + (-100 * k)))
+            new_turtle.shapesize(4.9)
+            squares_list.append(new_turtle)
+            
 
+    return squares_list
+
+def draw_x(x, y):
+    goto_without_pen(board_turtle, x, y)
+    board_turtle.setheading(45)
+    board_turtle.forward(40)
+    board_turtle.backward(80)
+
+    goto_without_pen(board_turtle, x, y)
+    board_turtle.setheading(315)
+    board_turtle.forward(40)
+    board_turtle.backward(80)
     pass
+
+def draw_o(x, y):
+    board_turtle.setheading(0)
+    goto_without_pen(board_turtle, x, (y-40))
+    board_turtle.circle(40)
+    pass
+
--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 import turtle
 import board
 
+turtle_spots_taken = []
+
+shape = "x"
+
+def handle_game(turtles_list):
+    def on_click(x, y):
+        global shape
+        
+        print("on click")
+        print(x, y)
+        correct_turtle = None
+
+        for turtle in turtles_list:
+            if x >= turtle.xcor() - 50 and x <= turtle.xcor() + 50:
+                if y >= turtle.ycor() - 50 and y <= turtle.ycor() + 50:
+                    correct_turtle = turtle
+                    break
+
+        correct_turtle.hideturtle()
+        turtle_spots_taken.append(correct_turtle)
+        if shape == "x":
+            board.draw_x(correct_turtle.xcor(), correct_turtle.ycor())
+            shape = "o"
+        else:
+            board.draw_o(correct_turtle.xcor(), correct_turtle.ycor())
+            shape = "x"
+
+        pass
+
+    for turtle in turtles_list:
+        turtle.onclick(on_click)
+
 
 
 def main():
     screen = turtle.Screen()
-    board.create_board()
+    turtles_list = board.create_board()
+    handle_game(turtles_list)
 
     screen.mainloop()
     pass
